6e6929004ebdb53c6ac64dbc51ee80e9f8635b2f
guillermopizarro <gpizarro@espol.edu.ec>
2024-11-06 08:10:01 -0500

    Protocolo UDP

--- unidad_03/udp/ejemplo_01/src/Receiver.java
+++ unidad_03/udp/ejemplo_01/src/Receiver.java
@@ -1,2 +1,45 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
 public class Receiver {
+
+    public static final int PORT = 2020;
+
+    public Receiver() {
+        try {
+            DatagramSocket socket = new DatagramSocket(PORT);
+        
+            // Para recibir mensajes
+            byte[] buffer = new byte[1500];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+
+            String mensaje = (new String(buffer)).trim();
+            System.out.println("Mensaje recibido: " + mensaje);
+
+            // Para enviar mensajes
+            Scanner consola = new Scanner(System.in);
+            System.out.print("Ingresar mensaje: ");
+            mensaje = consola.nextLine();
+
+            InetAddress sender_address = packet.getAddress();
+            int sender_port = packet.getPort();
+            buffer = mensaje.getBytes();
+
+            packet = new DatagramPacket(buffer, buffer.length, sender_address, sender_port);
+            socket.send(packet);
+            System.out.println("Mensaje enviado: " + mensaje);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Receiver objeto = new Receiver();
+    }
 }
--- unidad_03/udp/ejemplo_01/src/Sender.java
+++ unidad_03/udp/ejemplo_01/src/Sender.java
@@ -2,7 +2,6 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Scanner;
@@ -10,19 +9,28 @@ import java.util.Scanner;
 public class Sender {
     public Sender() {
         try {
-            DatagramSocket socket = DatagramSocket();
+            DatagramSocket socket = new DatagramSocket();
             
             Scanner consola = new Scanner(System.in);
             System.out.print("Ingresar mensaje: ");
             String mensaje = consola.nextLine();
             byte[] buffer = mensaje.getBytes();
             
+            // Para enviar mensajes
             InetAddress receiver_address = InetAddress.getByName("localhost");
             int receiver_port = 2020;
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length, receiver_address, receiver_port);
             
             socket.send(packet);
             System.out.println("Mensaje enviado: " + mensaje);
+
+             // Para recibir mensajes
+            buffer = new byte[1500];
+            packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+            
+            mensaje = (new String(buffer)).trim();
+            System.out.println("Mensaje recibido: " + mensaje);
         } catch (UnknownHostException ex) {
             System.out.println(ex.getMessage());
         } catch (IOException ex2) {
